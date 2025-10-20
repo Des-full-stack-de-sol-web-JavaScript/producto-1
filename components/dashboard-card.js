f936a5994e6f591dd1b9db2f090af25b1d4ace25
kevin <kevin.kafg@gmail.com>
2025-10-20 13:44:25 +0200

    Dashboard realizados

--- components/dashboard-card.js
+++ components/dashboard-card.js
@@ -1,7 +1,18 @@
-export function dashboardCard(){
-    const dashboardCard = document.createElement('div');
+export function dashboardCard({title, date, description, author, category, color}){
     
-    dashboardCard.innerHTML = ` 
+    const dashboardCard = document.createElement('div');
+    dashboardCard.className = 'col-12 col-md-6 col-lg-4';
+    dashboardCard.innerHTML = `
+
+        <div class="card h-100" style="background-color: ${color}; color: white;">
+            <div class="card-body">
+                <h5 class="card-title">${title}</h5>
+                <h6 class="card-subtitle mb-2"><strong>${date}</strong></h6>
+                <p class="card-text">${description}</p>
+                <p class="card-text"><strong>Publicado por: </strong> ${author}</p>
+                <p class="card-text"><strong>Categoría:</strong> ${category}</p>
+            </div>
+        </div>
 
     `;
     return dashboardCard;
--- logic/dashboard.js
+++ logic/dashboard.js
@@ -1,5 +1,17 @@
+import { dashboardData } from '../assets/data/dashboardData.js';
+import { dashboardCard } from '../components/dashboard-card.js'
+
+const contenedor = document.getElementById('dashboard');
+
 function dashboardPage() {
     console.log("Dashboard Page Loaded");
+    
+    contenedor.innerHTML = '';
+    
+    dashboardData.forEach(item => {
+        contenedor.appendChild(dashboardCard(item));
+
+    });
 
 }
 
